eecf888a935cf30e4387a0a740d1fd5d04bfc05c
Kahil <kahlilkmassicot@gmail.com>
2021-12-18 09:28:52 -0400

    test

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -4,7 +4,7 @@ export const CAMPSITES = [
         name: 'React Lake Campground',
         image: 'assets/images/react-lake.jpg',
         elevation: 1233,
-        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
+        description: "Nestled in the Foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
         comments: [
             {
                 id: 0,
