6573e3e172fca7be5ea4a88345136be6a1cc5f07
Kahil <kahlilkmassicot@gmail.com>
2021-12-24 21:53:47 -0400

    added react router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'typeface-open-sans';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
@@ -17,7 +16,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
